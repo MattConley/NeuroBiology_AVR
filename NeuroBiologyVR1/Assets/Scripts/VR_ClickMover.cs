d3cb1f03accf3dc61ed37743c0e513aea4afd47d
Matt Conley <Matt Conley>
2016-07-20 10:42:24 -0400

    Added smoother WASD movement, though it could be better

--- NeuroBiologyVR1/Assets/Scripts/VR_ClickMover.cs
+++ NeuroBiologyVR1/Assets/Scripts/VR_ClickMover.cs
@@ -5,27 +5,55 @@ public class VR_ClickMover : MonoBehaviour {
 
     public GameObject gvr_viewer; 
 
-    private Transform player_body;
+    private Transform player_transform;
     private Transform player_viewer;
+    private Rigidbody player_rbody;
     private Vector3 cam_direction;
     private int moveLen = 5;
+    private int moveSpeed = 10;
+    private float slowRate = (float)1.5;
 
 	// Use this for initialization
 	void Start () {
-        player_body = this.GetComponent<Transform>();
+        player_transform = this.GetComponent<Transform>();
+        player_rbody = this.GetComponent<Rigidbody>();
         player_viewer = gvr_viewer.GetComponent<Transform>();
-        cam_direction = player_body.forward;
-	}
+        cam_direction = player_transform.forward;
+        player_rbody.velocity = new Vector3(0, 0, 0);
+        Debug.Log("" + cam_direction);
+        player_rbody.useGravity = false;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         //cam_direction = player_body.forward;
-        
+        //Debug.Log("" + cam_direction);
         if (Input.GetKeyDown(KeyCode.W))
         {
             cam_direction = player_viewer.forward;
-            //Debug.Log("" + cam_direction * moveLen);
-            player_body.position = player_body.position + cam_direction * moveLen;
+            //player_transform.position = player_transform.position + cam_direction * moveLen;
+            player_rbody.velocity = new Vector3(cam_direction.x * moveSpeed, cam_direction.y * moveSpeed, cam_direction.z * moveSpeed);
+            //player_rbody.AddForce(cam_direction.x * moveSpeed, cam_direction.y * moveSpeed, cam_direction.z * moveSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            cam_direction = new Vector3(player_viewer.right.x*-1, player_viewer.right.y * -1, player_viewer.right.z * -1);
+            player_rbody.velocity = new Vector3(cam_direction.x * moveSpeed, cam_direction.y * moveSpeed, cam_direction.z * moveSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            cam_direction = player_viewer.right;
+            player_rbody.velocity = new Vector3(cam_direction.x * moveSpeed, cam_direction.y * moveSpeed, cam_direction.z * moveSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            cam_direction = new Vector3(player_viewer.forward.x * -1, player_viewer.forward.y * -1, player_viewer.forward.z * -1);
+            player_rbody.velocity = new Vector3(cam_direction.x * moveSpeed, cam_direction.y * moveSpeed, cam_direction.z * moveSpeed);
+        }
+        else
+        {
+            player_rbody.AddForce(player_rbody.velocity.x / -slowRate, player_rbody.velocity.y / -slowRate, player_rbody.velocity.z / -slowRate);
         }
 #if (UNITY_IPHONE)
         if (GvrController.ClickButton)
