ad881913b2197a254cd7ce8955e3b83dd932c22b
William Kodama <William Kodama>
2016-08-15 10:32:35 -0400

    8/15 Most Updated

---
+++ NeuroBiologyVR1/Assets/Scripts/AutomaticLoad.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutomaticLoad : MonoBehaviour {
+    static float currentTime = 0.0000f;
+    public int level;
+    // Use this for initialization
+    void Awake()
+    {
+        //BeginTimer
+        StartCoroutine(UpdateTime());
+    }
+    void Update()
+    {
+        StartCoroutine(UpdateTime());
+    }
+    private IEnumerator UpdateTime()
+    {
+        currentTime += Time.time;
+        
+        if(currentTime == 3.0)
+        {
+            Application.LoadLevel(level);
+            yield return null;
+        }
+    }
+}
