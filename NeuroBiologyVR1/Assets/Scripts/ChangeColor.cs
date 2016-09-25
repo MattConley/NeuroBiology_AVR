d20b0f242832ed1356787f7652b985966aa9bded
William Kodama <William Kodama>
2016-09-25 19:47:53 -0400

    Most Recent

---
+++ NeuroBiologyVR1/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+    public Renderer band;
+    public int rValue;
+    public int gValue;
+    public int bValue;
+
+    void Start()
+    {
+      Color bandColor = new Color(rValue, gValue, bValue, .75f);
+        band.material.color = bandColor;
+
+    }
+  
+}
