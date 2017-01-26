b56581838fe853459b1aa84671666f2871232c60
Matt Conley <Matt Conley>
2017-01-26 07:38:51 -0500

    Finally Updated

--- NeuroBiologyVR1/Assets/Graph_Maker/Examples/X_Tutorial_2/WMGTest.cs
+++ NeuroBiologyVR1/Assets/Graph_Maker/Examples/X_Tutorial_2/WMGTest.cs
@@ -2,68 +2,140 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class WMGTest : MonoBehaviour {
+public class WMGTest : MonoBehaviour
+{
 
-	public GameObject emptyGraphPrefab;
-	public GameObject CubeObject;
-	public WMG_Axis_Graph graph;
-	public WMG_Series series1;
-	public WMG_Series series2;
-    
+    public GameObject emptyGraphPrefab;
+    public GameObject CubeObject;
+    public WMG_Axis_Graph graph;
+    public WMG_Series series1;
+    public WMG_Series series2;
+
 
     private ChangeColor otherScript;
-	private double [,] newMatTwo;
-	private int x=20;
-	private int t=20;
-	private int recordingSite = 10;
-	
+    //private double [,] newMatTwo;
+    private double[] newMatTwo;
+    private int x = 20;
+    private int t = 20;
+    private int recordingSite = 10;
 
-	void Start () {
-		GameObject graphGO = GameObject.Instantiate(emptyGraphPrefab);
-		graphGO.transform.SetParent(this.transform, false);
-		graph = graphGO.GetComponent<WMG_Axis_Graph>();
-		series1 = graph.addSeries();
+    private float coroutine_time;
+    private float fupdate_time = 0.02f;
+    private float update_count = 0f;
+
+    public List<Vector2> data, data2;
+
+
+    void Start()
+    {
+        GameObject graphGO = GameObject.Instantiate(emptyGraphPrefab);
+        graphGO.transform.SetParent(this.transform, false);
+        graph = graphGO.GetComponent<WMG_Axis_Graph>();
+        series1 = graph.addSeries();
         series2 = graph.addSeries();
         series2.pointColor = new Color(0, 230, 0);
         series2.lineColor = new Color(0, 230, 0);
+
+        graph.xAxis.AxisMinValue = 20;
+        graph.xAxis.AxisMaxValue = 0;
+        graph.autoUpdateOrigin = false;
+        graph.theOrigin = new Vector2(0f, 0f);
+        graph.xAxis.AxisTitleString = "Time Passed (μs)";
+        graph.autoShrinkAtPercent = 0f;
+        graph.autoAnimationsEnabled = false;
+
+        coroutine_time = 0.15f;//otherScript.coroutine_wait;
+
+        otherScript = CubeObject.GetComponent<ChangeColor>();
+
+        data = new List<Vector2>();
+        data2 = new List<Vector2>();
+
         //graph.xAxis.AxisMaxValue = x;
 
     }
-	
 
-	public void RecieveSliderValue(float valuez){
-		recordingSite = (int)valuez;
 
-	}
+    public void RecieveSliderValue(float valuez)
+    {
+        recordingSite = (int)valuez;
 
-	// Use this for initialization
- void Update () {
-		//otherScript = CubeObject.GetComponent<ChangeColor>();
-		
-		//VTwo = otherScript.V;
-		//
-		//Refresh();
-		otherScript = CubeObject.GetComponent<ChangeColor>();
-        recordingSite = otherScript.recordingSite;
-        otherScript.UpdateRecorders();
-		newMatTwo = otherScript.newMat;
-		List<Vector2> data = new List<Vector2>();
-        List<Vector2> data2 = new List<Vector2>();
+    }
 
-        for (int i=0; i<t-1; i++){
+    // Use this for initialization
+    void Update()
+    {
 
-			data.Add(new Vector2((float)i, (float) newMatTwo[i,20]));
-		}
+        if (update_count >= coroutine_time)
+        {
+            update_count = update_count % coroutine_time;
 
-        series1.pointValues.SetList(data);
+            //otherScript = CubeObject.GetComponent<ChangeColor>();
 
-        for (int i=0; i<t-1; i++){
+            //VTwo = otherScript.V;
+            //
+            //Refresh();
 
-			data2.Add(new Vector2((float)i, (float) newMatTwo[i,x + recordingSite]));		
-		}
 
-        series2.pointValues.SetList(data2);
-        //print(recordingSite);
+            otherScript.UpdateRecorders();
+            recordingSite = otherScript.recordingSite;
+
+            //newMatTwo = otherScript.newMat;
+            newMatTwo = otherScript.cont_voltage;
+
+            //List<Vector2> data = new List<Vector2>();
+            //List<Vector2> data2 = new List<Vector2>();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+
+                float temp_x = data[i].x;
+                //Debug.Log(temp_x);
+                data.Insert(i, new Vector2(temp_x + 1, data[i].y));
+                data.RemoveAt(i + 1);
+                //data[i].Set(temp_x + 1, data[i].y);
+                //data2[i].Set(temp_x + 1, data2[i].y);
+                data2.Insert(i, new Vector2(temp_x + 1, data2[i].y));
+                data2.RemoveAt(i + 1);
+            }
+
+            data.Insert(0, new Vector2(0f, (float)newMatTwo[20]));
+            data2.Insert(0, new Vector2(0f, (float)newMatTwo[x + recordingSite]));
+
+            /**/
+            if (data.Count >= t)
+            {
+
+                data.RemoveAt(t - 1);
+                data2.RemoveAt(t - 1);
+            }/**/
+
+            series1.pointValues.SetList(data);
+            series2.pointValues.SetList(data2);
+            /*
+            for (int i = 0; i < t - 1; i++)
+            {
+
+                //data.Add(new Vector2((float)i, (float)newMatTwo[i, 20]));
+                data.Add(new Vector2((float)i, (float)newMatTwo[i, 20]));
+            }
+            
+            series1.pointValues.SetList(data);
+
+            for (int i = 0; i < t - 1; i++)
+            {
+
+                data2.Add(new Vector2((float)i, (float)newMatTwo[i, x + recordingSite]));
+            }
+
+            series2.pointValues.SetList(data2);
+            */
+
+        }
+        else
+        {
+            update_count += fupdate_time;
+        }
     }
 
 }
--- NeuroBiologyVR1/Assets/Scripts/BandsScalingScripts/ChangeColor.cs
+++ NeuroBiologyVR1/Assets/Scripts/BandsScalingScripts/ChangeColor.cs
@@ -6,24 +6,30 @@ using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
 
-public class ChangeColor : MonoBehaviour {
+public class ChangeColor : MonoBehaviour
+{
 
     // Grapher 1 Component
-	[Range(10, 100)]
-	public int resolution = 10;
+    [Range(10, 100)]
+    public int resolution = 10;
 
-	private int currentResolution;
-	private ParticleSystem.Particle[] points;
+    private int currentResolution;
+    private ParticleSystem.Particle[] points;
     // End of Grapher 1
 
-    public bool isStimulating;
-	public float multiplier;
-	public int rowi = 0;
-	public int coli = 0;
+    public float coroutine_wait = 0.15f;
 
-	public ArrayList x;
-	public ArrayList t;
-	ArrayList v0 = new ArrayList();
+    public bool isStimulating;
+    private bool isDone = false;
+    public float multiplier;
+    public int rowi = 0;
+    public int coli = 0;
+    public int cont_index = 0;
+
+
+    public ArrayList x;
+    public ArrayList t;
+    ArrayList v0 = new ArrayList();
 
 
     public GameObject stim_pipe, rec_pipe;
@@ -31,102 +37,117 @@ public class ChangeColor : MonoBehaviour {
     private float stim_z;
     public int recordingSite = 10;
 
-	//public List<GameObject> Bands;
-	public List<Renderer> FinalBands;
+    //public List<GameObject> Bands;
+    public List<Renderer> FinalBands;
 
-	//Cell Paremeters
+    //Cell Paremeters
 
-	float Ri = 36f;                 //Ohms/cm
-	float Cm = 1 * Mathf.Pow(10, -6);         //F/cm^2
-	float Rm = 1 / (36 * Mathf.Pow(10, -3));  //cm^2/S (Ohms*cm^2)
-	public double[,] newMat;
-	public double[,] V;
-	//Matrix 1:11
-	//101:11
+    float Ri = 36f;                 //Ohms/cm
+    float Cm = 1 * Mathf.Pow(10, -6);         //F/cm^2
+    float Rm = 1 / (36 * Mathf.Pow(10, -3));  //cm^2/S (Ohms*cm^2)
+    public double[,] newMat;
+    public double[,] V;
+    public double[] cont_voltage;      //for temporary use with continuous graph
+    private double[] zero_vec;          //used for fast reset of cont_voltage
+    //Matrix 1:11
+    //101:11
 
-	static Text timeText;
+    static Text timeText;
 
-	static float time = 0f;
+    static float time = 0f;
 
-	private float objScale = 1;
+    private float objScale = 1;
 
-	public static float inc = 0.0f;
-	void Start()
-	{
+    public static float inc = 0.0f;
+    void Start()
+    {
 
-		timeText = GameObject.FindGameObjectsWithTag("Timer")[0].GetComponent<Text>();
+        timeText = GameObject.FindGameObjectsWithTag("Timer")[0].GetComponent<Text>();
 
-		//Setting Up Array
-		//Bands = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Bands"));
-		//foreach (GameObject band in Bands) {
-			//FinalBands.Add (band.GetComponent<Renderer> ());
-		//}
-		
-//Space and Time Parameters
-		float dx = 100f * Mathf.Pow (10, -6);
+        //Setting Up Array
+        //Bands = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Bands"));
+        //foreach (GameObject band in Bands) {
+        //FinalBands.Add (band.GetComponent<Renderer> ());
+        //}
 
-		x = new ArrayList ();
-		float delta = 0.00005f;
-		float maxVal = 0.001f;
-		for (float i = 0; i < maxVal; i += delta) {
-			x.Add (i);
-		}
-		
+        //Space and Time Parameters
+        float dx = 100f * Mathf.Pow(10, -6);
 
-		t = new ArrayList ();
-		delta = .000001f;
-		maxVal = .000020f;
-		for (float i = delta; i < maxVal; i += delta) {
-			t.Add (i);
-		}
+        x = new ArrayList();
+        float delta = 0.00005f;
+        float maxVal = 0.001f;
+        for (float i = 0; i < maxVal; i += delta)
+        {
+            x.Add(i);
+        }
 
-		float q0 = 1f;
-		float diameter = Mathf.Pow(10, -6);   //Diameter
-		float ri = 4f * Ri / (Mathf.PI * Mathf.Pow (diameter, 2f));
-		float cm = Cm * Mathf.PI * diameter;
-		float rm = Rm / (Mathf.PI * diameter);
-		float lamda = 3f*Mathf.Sqrt (rm / ri)/2f; // Space Constant
-		float tau = rm * cm; //Time Constant
-		double max_Val = (q0 / (2 * cm * lamda * Mathf.Sqrt (Mathf.PI * (float)t [0] / tau))) * Mathf.Exp ((-1.0f * Mathf.Pow ((float)x [0] / lamda, 2)-4.0f * Mathf.Pow ((float)t [0] / tau, 2)) / (4.0f * (float)t [0] / tau));;
 
-		diameter = multiplier * Mathf.Pow(10, -6);   //Diameter
-		ri = 4f * Ri / (Mathf.PI * Mathf.Pow (diameter, 2f));
-		cm = Cm * Mathf.PI * diameter;
-		rm = Rm / (Mathf.PI * diameter);
-		lamda = 3f*Mathf.Sqrt (rm / ri)/2f; // Space Constant
-		tau = rm * cm; //Time Constant
-		
-		V = new double[t.Count, x.Count];
+        t = new ArrayList();
+        delta = .000001f;
+        maxVal = .000020f;
+        for (float i = delta; i < maxVal; i += delta)
+        {
+            t.Add(i);
+        }
 
-		for (int i = 0; i < x.Count; i++) {
-			for (int row = 0; row < t.Count; row++) {
-				V [row, i] = (q0 / (2 * cm * lamda * Mathf.Sqrt (Mathf.PI * (float)t [row] / tau))) * Mathf.Exp ((-1.0f * Mathf.Pow ((float)x [i] / lamda, 2)-4.0f * Mathf.Pow ((float)t [row] / tau, 2)) / (4.0f * (float)t [row] / tau)); 
-			}
+        float q0 = 1f;
+        float diameter = Mathf.Pow(10, -6);   //Diameter
+        float ri = 4f * Ri / (Mathf.PI * Mathf.Pow(diameter, 2f));
+        float cm = Cm * Mathf.PI * diameter;
+        float rm = Rm / (Mathf.PI * diameter);
+        float lamda = 3f * Mathf.Sqrt(rm / ri) / 2f; // Space Constant
+        float tau = rm * cm; //Time Constant
+        double max_Val = (q0 / (2 * cm * lamda * Mathf.Sqrt(Mathf.PI * (float)t[0] / tau))) * Mathf.Exp((-1.0f * Mathf.Pow((float)x[0] / lamda, 2) - 4.0f * Mathf.Pow((float)t[0] / tau, 2)) / (4.0f * (float)t[0] / tau)); ;
 
-		}
-		
+        diameter = multiplier * Mathf.Pow(10, -6);   //Diameter
+        ri = 4f * Ri / (Mathf.PI * Mathf.Pow(diameter, 2f));
+        cm = Cm * Mathf.PI * diameter;
+        rm = Rm / (Mathf.PI * diameter);
+        lamda = 3f * Mathf.Sqrt(rm / ri) / 2f; // Space Constant
+        tau = rm * cm; //Time Constant
 
-		newMat = new double[t.Count, (x.Count - 1) * 2 + 1];
+        V = new double[t.Count, x.Count];
 
-		for (int row = 0; row < t.Count; row++) {
-			for (int col = 0; col < x.Count - 1; col++) {
+        for (int i = 0; i < x.Count; i++)
+        {
+            for (int row = 0; row < t.Count; row++)
+            {
+                V[row, i] = (q0 / (2 * cm * lamda * Mathf.Sqrt(Mathf.PI * (float)t[row] / tau))) * Mathf.Exp((-1.0f * Mathf.Pow((float)x[i] / lamda, 2) - 4.0f * Mathf.Pow((float)t[row] / tau, 2)) / (4.0f * (float)t[row] / tau));
+            }
 
-				newMat [row, col+x.Count] = V [row, col+1];
-				newMat [row, (x.Count - 1) - col] = V [row, col];
-			}
-			newMat [row, x.Count - 1] = V [row, 0];
-			newMat [row, 0] = V [row, x.Count - 1];
-		}
-		
-		
+        }
 
-		//normalize matrix
-		for (int row = 0; row < t.Count; row++) {
-			for (int col = 0; col < (x.Count - 1) * 2+1; col++) {
-				newMat [row, col] = newMat [row, col] / max_Val;
-				
-			}
-		}
+
+        newMat = new double[t.Count, (x.Count - 1) * 2 + 1];
+        cont_voltage = new double[(x.Count - 1) * 2 + 1];
+        zero_vec = new double[(x.Count - 1) * 2 + 1];
+
+        for (int row = 0; row < t.Count; row++)
+        {
+            for (int col = 0; col < x.Count - 1; col++)
+            {
+
+                newMat[row, col + x.Count] = V[row, col + 1];
+                newMat[row, (x.Count - 1) - col] = V[row, col];
+
+            }
+
+            newMat[row, x.Count - 1] = V[row, 0];
+            newMat[row, 0] = V[row, x.Count - 1];
+        }
+
+
+
+        //normalize matrix
+        for (int row = 0; row < t.Count; row++)
+        {
+            for (int col = 0; col < (x.Count - 1) * 2 + 1; col++)
+            {
+                newMat[row, col] = newMat[row, col] / max_Val;
+                cont_voltage[col] = 0;     //assuming Vo = 0
+                zero_vec[col] = 0;
+            }
+        }
 
 
         //Handling Electrode Locations
@@ -137,109 +158,141 @@ public class ChangeColor : MonoBehaviour {
 
         UpdateRecorders();
 
-		//Create New Points for Graph
-		CreatePoints();
-	}
+        //Create New Points for Graph
+        CreatePoints();
+    }
+
+    public void UpdateVoltage()
+    {
+
+    }
 
     public void UpdateRecorders()
     {
         rec_pipe.GetComponent<Transform>().position = new Vector3(rec_pipe.GetComponent<Transform>().position.x, rec_pipe.GetComponent<Transform>().position.y, stim_z - recordingSite * delta_z);
     }
 
-	public void CreatePoints () {
+    public void CreatePoints()
+    {
 
-		if (resolution < 10 || resolution > 100) {
-			Debug.LogWarning("Grapher resolution out of bounds, resetting to minimum.", this);
-			resolution = 10;
-		}
-		currentResolution = resolution;
-		points = new ParticleSystem.Particle[resolution];
-		float increment = 1f / (resolution - 1);
-		for(int i = 0; i < resolution; i++){
-			float x = i * 205f*increment;
-			points[i].position = new Vector3(x, 0f, 0f);
-			points[i].color = new Color(0f, 0f, 0f);
-			points[i].size = 2f;
-		}
-	}
+        if (resolution < 10 || resolution > 100)
+        {
+            Debug.LogWarning("Grapher resolution out of bounds, resetting to minimum.", this);
+            resolution = 10;
+        }
+        currentResolution = resolution;
+        points = new ParticleSystem.Particle[resolution];
+        float increment = 1f / (resolution - 1);
+        for (int i = 0; i < resolution; i++)
+        {
+            float x = i * 205f * increment;
+            points[i].position = new Vector3(x, 0f, 0f);
+            points[i].color = new Color(0f, 0f, 0f);
+            points[i].size = 2f;
+        }
+    }
 
-/*
-	void Update(){
+    /*
+        void Update(){
 
-		if(Input.GetMouseButtonDown(0)){
-			//isStimulating = false;
-			isStimulating = true;
-			rowi = 0;
-			coli = 0;
-			StartCoroutine (FadetoClear());
-		}
-
-
-	}
-	*/
-
-	public void OnTrigger(){
-
-			isStimulating = true;
-			rowi = 0;
-			coli = 0;
-			StartCoroutine (FadetoClear());
+            if(Input.GetMouseButtonDown(0)){
+                //isStimulating = false;
+                isStimulating = true;
+                rowi = 0;
+                coli = 0;
+                StartCoroutine (FadetoClear());
+            }
 
 
-	}
+        }
+        */
 
-	public void SetVariable(float value){
+    public void OnTrigger()
+    {
 
-		multiplier = value;
-		Start();
+        if (isDone)
+        {
+            StopCoroutine(FadetoClear());
+        }
+        isDone = false;
+        isStimulating = true;
+        rowi = 0;
+        coli = 0;
+        StartCoroutine(FadetoClear());
 
-	}
 
-	public IEnumerator FadetoClear()
-	{
+    }
 
-		while (isStimulating == true) 
-		{
-			for (rowi = 0; rowi < t.Count; rowi++) {
-				for (coli = 0; coli < (x.Count - 1) * 2 + 1; coli++) {
-					Color bandColor = new Color ((float)newMat [rowi, coli], 0, 1f - (float)newMat [rowi, coli], .75f);
-					FinalBands [coli].material.color = bandColor;
-					//Audio Component					
-					/*
-					AudioSource audio;
-					audio = GetComponent<AudioSource>();
-					audio.pitch = 1.05946f + (float)newMat [rowi,20];
-					audio.Play();
-*/
-				}
-               
+    public void SetVariable(float value)
+    {
 
-                inc = (float)newMat [rowi,x.Count + recordingSite];
-                AudioSource audio;
-                audio = GetComponent<AudioSource>();  
-                if(inc>0){
-                	audio.pitch = 1+Mathf.Lerp(0,(float)newMat [0,x.Count + recordingSite], inc);
-					audio.Play();
-					inc += .5f * Time.deltaTime;
+        multiplier = value;
+        Start();
+
+    }
+
+    public IEnumerator FadetoClear()
+    {
+        while (isStimulating == true)
+        {
+
+            if (rowi >= t.Count)
+            {
+                isStimulating = false;
+                isDone = true;
+                cont_voltage = zero_vec;
+                yield return new WaitForSeconds(coroutine_wait);
+            }
+            else
+            {
+                for (coli = 0; coli < (x.Count - 1) * 2 + 1; coli++)
+                {
+                    Color bandColor = new Color((float)newMat[rowi, coli], 0, 1f - (float)newMat[rowi, coli], .75f);
+                    FinalBands[coli].material.color = bandColor;
+                    cont_voltage[coli] = newMat[rowi, coli];
+
+                    //Audio Component					
+                    /*
+                    AudioSource audio;
+                    audio = GetComponent<AudioSource>();
+                    audio.pitch = 1.05946f + (float)newMat [rowi,20];
+                    audio.Play();
+                    */
                 }
 
 
-				if (currentResolution != resolution) {
-					CreatePoints();
-				}
+                inc = (float)newMat[rowi, x.Count + recordingSite];
+                AudioSource audio;
+                audio = GetComponent<AudioSource>();
+                if (inc > 0)
+                {
+                    audio.pitch = 1 + Mathf.Lerp(0, (float)newMat[0, x.Count + recordingSite], inc);
+                    audio.Play();
+                    inc += .5f * Time.deltaTime;
+                }
 
-				for (int i = 0; i < resolution; i++) {
-					Vector3 p = points[i].position;
-					p.y = 10f*(float)newMat[rowi,i];
-					points[i].position = p;
-				}
-			
-				GetComponent<ParticleSystem>().SetParticles(points, points.Length);
 
-				isStimulating = false;
-				//timeText.text = Mathf.Round(rowi*delta*1e6f) + "μs";
-				yield return new WaitForSeconds (.15f);
-			}
-		}
-	}
+                if (currentResolution != resolution)
+                {
+                    CreatePoints();
+                }
+
+                for (int i = 0; i < resolution; i++)
+                {
+                    Vector3 p = points[i].position;
+                    p.y = 10f * (float)newMat[rowi, i];
+                    points[i].position = p;
+                }
+
+                GetComponent<ParticleSystem>().SetParticles(points, points.Length);
+
+
+                rowi++;
+                //isStimulating = false;
+                //timeText.text = Mathf.Round(rowi*delta*1e6f) + "μs";
+                yield return new WaitForSeconds(coroutine_wait);
+            }
+            //}
+        }
+    }
 }
